98d4f4f10f00e536ca24d9cf37392d171d288b44
Administrator <admin@example.com>
2019-01-17 14:20:46 +0800

    测试GIT提交

--- calculate/compare.go
+++ calculate/compare.go
@@ -1,7 +1,8 @@
 /*
-@Time : 2019-01-16 13:42 
+@Time : 2019-01-16 13:42
 @Author : caozg
 @File : compare
+测试GIT提交
 */
 package main
 
@@ -11,29 +12,29 @@ func main() {
 	var a int = 21
 	var b int = 10
 
-	if( a == b ) {
-		fmt.Printf("第一行 - a 等于 b\n" )
+	if a == b {
+		fmt.Printf("第一行 - a 等于 b\n")
 	} else {
-		fmt.Printf("第一行 - a 不等于 b\n" )
+		fmt.Printf("第一行 - a 不等于 b\n")
 	}
-	if ( a < b ) {
-		fmt.Printf("第二行 - a 小于 b\n" )
+	if a < b {
+		fmt.Printf("第二行 - a 小于 b\n")
 	} else {
-		fmt.Printf("第二行 - a 不小于 b\n" )
+		fmt.Printf("第二行 - a 不小于 b\n")
 	}
 
-	if ( a > b ) {
-		fmt.Printf("第三行 - a 大于 b\n" )
+	if a > b {
+		fmt.Printf("第三行 - a 大于 b\n")
 	} else {
-		fmt.Printf("第三行 - a 不大于 b\n" )
+		fmt.Printf("第三行 - a 不大于 b\n")
 	}
 	/* Lets change value of a and b */
 	a = 5
 	b = 20
-	if ( a <= b ) {
-		fmt.Printf("第四行 - a 小于等于 b\n" )
+	if a <= b {
+		fmt.Printf("第四行 - a 小于等于 b\n")
 	}
-	if ( b >= a ) {
-		fmt.Printf("第五行 - b 大于等于 a\n" )
+	if b >= a {
+		fmt.Printf("第五行 - b 大于等于 a\n")
 	}
 }
